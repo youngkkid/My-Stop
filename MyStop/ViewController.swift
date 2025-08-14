763a59fc789ba45d2cbc97d11bec8d55c43044a0
IlyaAnufriev <ilya.anufriev2014@yandex.ru>
2025-08-14 10:18:05 +0300

    Initial Commit

---
+++ MyStop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStop
+//
+//  Created by Илья Ануфриев on 14.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
