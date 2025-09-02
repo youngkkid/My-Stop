a0f212da8649b7c4870b4bde569aaa61203631db
IlyaAnufriev <ilya.anufriev2014@yandex.ru>
2025-09-02 11:50:44 +0300

    refactor: configuring the application lifecycle

--- MyStop/SceneDelegate.swift
+++ MyStop/SceneDelegate.swift
@@ -1,43 +1,50 @@
-//
-//  SceneDelegate.swift
-//  MyStop
-//
-//  Created by Илья Ануфриев on 14.08.2025.
-//
-
 import UIKit
+import UserNotifications
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let mainVC = BusStopViewController()
+        let manager = BusStopViewModel()
+        let mainVC = BusStopViewController(manager: manager )
         let navController = UINavigationController(rootViewController: mainVC)
         window.rootViewController = navController
         self.window = window
         window.makeKeyAndVisible()
+        
+        requestNotificationPermission()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
+    
+    private func requestNotificationPermission() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) {granted, error in
+            if let error = error {
+                print("Ошибка при запросе разрешения: \(error)")
+            } else {
+                print("Разрешение на уведомления: \(granted)")
+            }
+        }
     }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    
+    func sceneWillResignActive(_ scene: UIScene) {}
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
+extension SceneDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound])
     }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
-
-
 }
 
